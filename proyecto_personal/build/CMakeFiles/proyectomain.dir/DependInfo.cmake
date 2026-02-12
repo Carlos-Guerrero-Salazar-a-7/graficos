b1f6891c594994d2deab8bd405371f760a7af2e1
Carlos <carlos@Compu.de.carlos>
2026-02-12 13:18:16 -0600

    fuck

---
+++ proyecto_personal/build/CMakeFiles/proyectomain.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/carlos/github/proyecto_personal/lib/glad.c" "CMakeFiles/proyectomain.dir/lib/glad.c.o" "gcc" "CMakeFiles/proyectomain.dir/lib/glad.c.o.d"
+  "/home/carlos/github/proyecto_personal/src/nuevo.cpp" "CMakeFiles/proyectomain.dir/src/nuevo.cpp.o" "gcc" "CMakeFiles/proyectomain.dir/src/nuevo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proyecto_personal/build/CMakeFiles/proyectomain.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectomain.dir/lib/glad.c.o"
+  "CMakeFiles/proyectomain.dir/lib/glad.c.o.d"
+  "CMakeFiles/proyectomain.dir/src/nuevo.cpp.o"
+  "CMakeFiles/proyectomain.dir/src/nuevo.cpp.o.d"
+  "bin/proyectomain"
+  "bin/proyectomain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/proyectomain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
